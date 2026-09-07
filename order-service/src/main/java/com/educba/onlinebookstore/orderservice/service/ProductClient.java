028cb81782da851d398d93f4100b623bcd887751
NMWork <netrikam@gmail.com>
2026-09-07 11:47:35 +0530

    feat(serviceCommunication): Added RestClient

---
+++ order-service/src/main/java/com/educba/onlinebookstore/orderservice/service/ProductClient.java
@@ -0,0 +1,26 @@
+package com.educba.onlinebookstore.orderservice.service;
+
+import com.educba.onlinebookstore.orderservice.dto.ProductResponse;
+import com.educba.onlinebookstore.orderservice.exception.ProductNotFoundException;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+public class ProductClient {
+
+    private final RestClient restClient;
+
+    public ProductClient(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder .baseUrl("http://localhost:8081") .build();
+    }
+
+    public ProductResponse getProduct(Long id) {
+        return restClient.get()
+                .uri("/api/products/{id}", id)
+                .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> { throw new ProductNotFoundException(id); })
+                .body(ProductResponse.class);
+    }
+
+}
